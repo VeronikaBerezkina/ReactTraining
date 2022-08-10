a94eedda2f05e6453d8d189ffebff675db108f51
Veronika <veronikaberezkina18@gmail.com>
2022-08-10 22:30:46 +0300

    imitial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  //
+  const [click, setClick] = useState();
+
+  //
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+        <button>Click me</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
